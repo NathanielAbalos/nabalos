024b5f3f31599d7ed3754cb790daf196af410152
NathanielAbalos <143874233+NathanielAbalos@users.noreply.github.com>
2023-11-03 09:29:46 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", (event)=>{
+ 
+    nav.classList.toggle("show");     
+ 
+    
+});
+
+
+
